9ec4b19fdc7176c550bdffa2586ac250693a9c1d
Soham Bangar <sohambangar188@gmail.com>
2026-09-01 23:55:13 +0530

    customer support is add and clickjack vulnerability is fixed

---
+++ frontend/lib/constants.ts
@@ -0,0 +1,2 @@
+
+export const SUPPORT_EMAIL = "Studenterpsyssupport@gmail.com";
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Content-Security-Policy", value: "frame-ancestors 'none';" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
